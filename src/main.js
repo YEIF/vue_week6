70b55267e514c0a2843064c21b569eb85edd123d
IF <ast61006@gmail.com>
2022-03-02 11:22:01 +0800

    增加 vee-validate 套件

--- src/main.js
+++ src/main.js
@@ -4,9 +4,40 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
+import { Form, Field, ErrorMessage, defineRule, configure } from 'vee-validate'
+import AllRules from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import ZhTw from './zh_TW.js'
 
-// createApp(App).use(router, VueAxios, axios).mount('#app')
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule])
+})
+
+configure({
+  generateMessage: localize({ zh_TW: ZhTw }),
+  validateOnInput: true
+})
+setLocale('zh_TW')
+
+/* ray start */
+// import { localize, setLocale } from '@vee-validate/i18n'
+// import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+// Object.keys(AllRules).forEach((rule) => {
+//   defineRule(rule, AllRules[rule])
+// })
+
+// configure({
+//   generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+//   validateOnInput: true // 當輸入任何內容直接進行驗證
+// })
+// // 設定預設語系
+// setLocale('zh_TW')
+/* ray end */
+
 const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
+app.component('VForm', Form)
+app.component('VField', Field)
+app.component('ErrorMessage', ErrorMessage)
 app.mount('#app')
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -55,6 +55,48 @@
       </tr>
     </tfoot>
   </table>
+
+      <div class="my-5 row justify-content-center">
+        <VForm ref="form" class="col-md-6" v-slot="{ errors }" @submit="sendOrder">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <VField id="email" name="email" type="email" class="form-control"
+              :class="{ 'is-invalid': errors['email'] }" placeholder="請輸入 Email" rules="required|email"
+              v-model="form.user.email"> </VField>
+            <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="name" class="form-label">收件人姓名</label>
+            <VField id="name" name="姓名" type="text" class="form-control" :class="{ 'is-invalid': errors['姓名'] }"
+              placeholder="請輸入姓名" rules="required" v-model="form.user.name"></VField>
+            <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="tel" class="form-label">收件人電話</label>
+            <VField id="tel" name="電話" type="tel" class="form-control" :class="{ 'is-invalid': errors['電話'] }"
+              placeholder="請輸入電話" rules="required|min:8|max:10|numeric" v-model="form.user.tel"></VField>
+            <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="address" class="form-label">收件人地址</label>
+            <VField id="address" name="地址" type="text" class="form-control" :class="{ 'is-invalid': errors['地址'] }"
+              placeholder="請輸入地址" rules="required" v-model="form.user.address"></VField>
+            <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="message" class="form-label">留言</label>
+            <textarea id="message" class="form-control" cols="30" rows="10" v-model="form.user.message"></textarea>
+          </div>
+          <div class="text-end">
+            <button type="submit" class="btn btn-danger"
+              :disabled="Object.keys(errors).length > 0 || carts.carts?.length ===0">送出訂單</button>
+          </div>
+        </VForm>
+      </div>
 </template>
 <script>
 import emitter from '@/libs/emitter'
@@ -62,7 +104,16 @@ export default {
   data () {
     return {
       carts: {},
-      isLoadingItem: ''
+      isLoadingItem: '',
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: '',
+          message: ''
+        }
+      }
     }
   },
   methods: {
@@ -115,6 +166,19 @@ export default {
         .catch((err) => {
           console.dir(err)
         })
+    },
+    sendOrder () {
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/order`
+      const order = this.form
+      this.$http.post(url, { data: order }).then((response) => {
+        alert(response.data.message)
+        this.$refs.form.resetForm()
+        this.form.user.message = ''
+        this.getCarts()
+      }).catch((err) => {
+        console.dir(err)
+        alert(err.response.data.message)
+      })
     }
   },
   mounted () {
